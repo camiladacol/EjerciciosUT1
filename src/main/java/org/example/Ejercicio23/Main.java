b37875d3a0c694f6bc6f7817f487c17cca42d624
camiladacol <camila.dacol@correo.ucu.edu.uy>
2026-04-06 21:03:17 -0300

    Cambio en main ejercicio 23

--- src/main/java/org/example/Ejercicio23/Main.java
+++ src/main/java/org/example/Ejercicio23/Main.java
@@ -5,17 +5,14 @@ public class Main
     public static void main(String[] args)
     {
         Empresa empresa1 = new Empresa();
-        cargarArchivo (empresa1, "suc1.txt");
+        Prueba.cargarArchivo (empresa1, "suc1.txt");
+
         System.out.println("Cantidad de sucursales:" + empresa1.cantidadSucursales());
         empresa1.removerSucursal("Chicago"); //Eliminar Chicago de la lista
 
         Empresa empresa2 = new Empresa();
-        cargarArchivo (empresa2, "suc2.txt");
+        Prueba.cargarArchivo (empresa2, "suc2.txt");
 
     }
 
-    private static void cargarArchivo(Empresa empresa1, String nombreArchivo)
-    {
-
-    }
 }
---
+++ src/main/java/org/example/Ejercicio23/Prueba.java
@@ -0,0 +1,9 @@
+package org.example.Ejercicio23;
+
+public class Prueba
+{
+    static void cargarArchivo(Empresa empresa1, String nombreArchivo)
+    {
+
+    }
+}

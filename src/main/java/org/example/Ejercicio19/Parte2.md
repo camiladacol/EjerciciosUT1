9655006848acf25dc5bfbb0650d3cbc5ad71af85
camiladacol <camila.dacol@correo.ucu.edu.uy>
2026-04-13 15:52:42 -0300

    Ejercicio 19 completo

--- src/main/java/org/example/Ejercicio19/Parte2.md
+++ src/main/java/org/example/Ejercicio19/Parte2.md
@@ -11,6 +11,19 @@ El peor caso posible es que nunca entre una llave de cierre y solo queden de abi
 
 En el caso de ir completando pares de llaves la memoria será poca, si son siempre abiertos se ocupará más.
 
-(agregar pre y post condiciones)
+**Precondiciones:**
+* La pila tiene que estar vacía 
+* La lista puede contener { y }
 
-revisar explicacion, ver ordenes
+**Postcondiciones:**
+* La función devuelve VERDADERO si la secuencia de llaves está bien formada
+* La lista de entrada no se modifica
+* La función devuelve FALSO si la secuencia de llaves no está bien formada
+* Si la expresión esta bien formada, la pila vuelve a quedar vacía
+
+Lenguaje natural
+-
+Se parte de una pila vacía y se recorre la lista de caracteres de entrada
+Cada vez que aparece una llave que abre {, se agrega en la pila
+Cada vez que aparece una llave de cierre }, se intenta sacar un elemento de la pila
+Al finalizar el recorrido, si la pila quedó vacía, la secuencia es correcta,en caso contrario, faltan cierres y la secuencia es incorrecta.
--- src/main/java/org/example/Ejercicio19/Seudocodigo19.java
+++ src/main/java/org/example/Ejercicio19/Seudocodigo19.java
@@ -4,25 +4,25 @@ public class Seudocodigo19
 {
     /*
     Función controlCorchetes (listaEntrada)
-        crear pila vacia //Se crea la pila donde se van a guardar las llaves abiertas
-        para cada caracter x en listaEntrada hacer //Se recorre la lista //Orden N
-            si x = "{" entonces //Si encuentra un {, lo guarda en la pila //Orden 1
-                apilar (x)
-            sino
-                si x "}" entonces //Se verifica si es una llave de cierre
-                    si pila esta vacia entonces
-                        devolver falso // Si aparece un } pero la pila está vacía, es falso
+O(1)        crear pila vacia //Se crea la pila donde se van a guardar las llaves abiertas
+O(n)          para cada caracter x en listaEntrada hacer //Se recorre la lista
+O(1)              si x = "{" entonces //Si encuentra un {, lo guarda en la pila
+O(1)                  apilar (x)
+                 sino
+O(1)                  si x = "}" entonces //Se verifica si es una llave de cierre
+O(1)                      si pila esta vacia entonces
+O(1)                          devolver falso // Si aparece un } pero la pila está vacía, es falso
                     sino
-                        sacar de la pila
+O(1)                          sacar de la pila
                     fin si
                 fin si
             fin si
          fin para
 
-         si pila esta vacia entonces //Si no quedo nada en la pila esta todo perfecto
-            devolver verdadero //Y devuelve verdadero
+O(1)           si pila esta vacia entonces //Si no quedo nada en la pila esta todo perfecto
+O(1)              devolver verdadero //Y devuelve verdadero
          sino
-            devolver falso //Sino devuelve falso, si quedaron { sin cerrar
+O(1)              devolver falso //Sino devuelve falso, si quedaron { sin cerrar
          fin si
      fin
      */
